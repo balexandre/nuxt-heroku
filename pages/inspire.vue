1d98970da6f2f53dfda528052ed56141dc348e03
Bruno Alexandre <bruno.in.dk@gmail.com>
2020-02-24 23:39:57 +0100

    added simple call to serverMiddleware

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -10,6 +10,59 @@
           </small>
         </footer>
       </blockquote>
+
+      <hr />
+      <v-row>
+        <v-col>
+          <v-btn @click.stop="fetchPostsByUser">Fetch Posts for User #1</v-btn>
+          <v-btn @click.stop="fetchUsers">Fetch All Users</v-btn></v-col
+        ></v-row
+      >
+      <hr />
+      <v-row
+        ><v-col>
+          <p v-if="loading">fetching data...</p>
+          <pre v-else class="text-left">{{ code }}</pre></v-col
+        ></v-row
+      >
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      code: null,
+      loading: false
+    }
+  },
+  methods: {
+    fetchPostsByUser() {
+      this.callBackendApi('/api/user/1/posts')
+    },
+    fetchUsers() {
+      this.callBackendApi('/api/users')
+    },
+    callBackendApi(url) {
+      const _ = this
+
+      this.loading = true
+      fetch(url)
+        .then((res) => res.json())
+        .then((json) => {
+          _.code = json
+          _.loading = false
+        })
+        .catch((err) => {
+          _.code = err.message + '\n\n' + err.stack
+          _.loading = false
+          // eslint-disable-next-line no-console
+          console.error(err)
+        })
+    }
+  }
+}
+</script>
+
+<style></style>
